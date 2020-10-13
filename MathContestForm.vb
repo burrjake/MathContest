66365672e000fa9f9fba13ad4450c0d32f9ad21c
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-10-13 08:28:31 -0600

    add age verification

--- MathContestForm.vb
+++ MathContestForm.vb
@@ -13,5 +13,22 @@ Public Class MathContestForm
         Me.Close()
     End Sub
 
+    Private Sub AgeBox_TextChanged(sender As Object, e As EventArgs) Handles AgeTextBox.Leave
+        Dim ageNumber As Integer
+
+        Try
+            ageNumber = CInt(AgeTextBox.Text)
+            If ageNumber >= 7 And ageNumber <= 11 Then
+                MsgBox("Welcome to the Math Contest")
+            ElseIf ageNumber < 7 And ageNumber > 11 Then
+                MsgBox("Student not eligible to complete")
+            End If
+        Catch ex As Exception
+            MsgBox("Please enter valid age")
+
+
+        End Try
+    End Sub
+
 
 End Class
